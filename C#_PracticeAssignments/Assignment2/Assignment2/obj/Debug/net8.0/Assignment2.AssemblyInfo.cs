464529626606c510ff8e40c785f4bcf6658dd9e5
Gokul-T-M <gokulmanoharan2805@gmail.com>
2025-03-31 23:10:36 +0530

    Practice Assignment 3 for C# Added

--- C#_PracticeAssignments/Assignment2/Assignment2/obj/Debug/net8.0/Assignment2.AssemblyInfo.cs
+++ C#_PracticeAssignments/Assignment2/Assignment2/obj/Debug/net8.0/Assignment2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cd545eace41f75b7303e4f39e0ccec374805d2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
